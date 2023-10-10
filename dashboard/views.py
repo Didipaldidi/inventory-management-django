b278a8239acf00f56de63b54239440e3eb305bda
Valdi <valdilaurentius@gmail.com>
2023-10-10 12:39:45 -0700

    added staff views in dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("This is the index page")
+
+
+def staff(request):
+    return HttpResponse("This is the Staff page")
