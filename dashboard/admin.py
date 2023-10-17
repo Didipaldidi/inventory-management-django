13161e200a7e5a28a84cc490d6ec4e2d73bea8d7
Valdi <valdilaurentius@gmail.com>
2023-10-17 12:30:18 -0700

    migrate Product model

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product
+from django.contrib.auth.models import Group
 
 # Register your models here.
+admin.site.register(Product)
--- dashboard/models.py
+++ dashboard/models.py
@@ -8,6 +8,9 @@ CATEGORY = (
 )
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length=20, choices=CATEGORY)
-    quantity = models.PositiveBigIntegerField()
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY, null=True)
+    quantity = models.PositiveBigIntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.quantity}'
