aca097b44dd5247aa6cf5c96c55fef38ed72b630
Valdi <valdilaurentius@gmail.com>
2023-10-19 19:33:35 -0700

    added the form for creating new product

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,30 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 @login_required(login_url='user-login')
-
 def index(request):
     return render(request, 'dashboard/index.html')
 
 @login_required(login_url='user-login')
-
 def staff(request):
     return render(request, 'dashboard/staff.html')
 
 @login_required(login_url='user-login')
-
 def product(request):
     items = Product.objects.all()
+
+    if request.method =="POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-product')
+    else:
+        form = ProductForm()
+        print("invalid form")
+
     context = {
-        'items': items
+        'items': items,
+        'form': form,
     }
 
     return render(request, 'dashboard/product.html', context)
 
 @login_required(login_url='user-login')
-
 def order(request):
     return render(request, 'dashboard/order.html')
